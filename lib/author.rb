8dcd6dbf639723b595f3ecf8c552567cc9f9dbc7
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-14 18:35:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
   
   attr_accessor :name
   
+  @@post_count = 0
+  
   def initialize(name)
     @name = name
     @posts = []
@@ -14,6 +16,7 @@ class Author
   def add_post(post)
     @posts << post 
     post.author = self 
-    
+    @@post_count += 1 
+  end
     
 end 
